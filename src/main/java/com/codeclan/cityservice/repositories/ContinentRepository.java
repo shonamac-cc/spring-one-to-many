f6851eac2112d8fa87515825fb608a9787c94e0c
Shona McKenzie <shona_mckenzie@hotmail.com>
2019-10-22 19:16:38 +0100

    rolled back repository changes, tested and works ok

--- src/main/java/com/codeclan/cityservice/repositories/ContinentRepository.java
+++ src/main/java/com/codeclan/cityservice/repositories/ContinentRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import org.springframework.stereotype.Repository;
 
-@RepositoryRestResource
+
+@Repository
 public interface ContinentRepository extends JpaRepository<Continent, Long> {
 }
